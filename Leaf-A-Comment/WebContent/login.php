51f0351293896caa06b19eefa02415563e764e9f
nathania4c <nathania4c@gmail.com>
2020-03-26 16:26:28 +0700

    # WARNING: head commit changed in the meantime
    Merge branch 'nats' of https://github.com/Ryanlys/Leaf-A-Comment.git
    into nats

--- Leaf-A-Comment/WebContent/login.php
+++ Leaf-A-Comment/WebContent/login.php
@@ -1,1 +1,41 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+
+    session_start();
+    
+    $mysqli = new mysqli("localhost", "root","", "test");
+    
+    if($mysqli -> connect_errno){
+        exit();
+    }
+    
+    $username = $_REQUEST["username"];
+    $password = $_REQUEST["password"];
+    $admin = false;
+    
+    $sql = "SELECT userid, username, password FROM users";
+    
+    if($result = $mysqli -> query($sql)){
+        while($fieldinfo = $result -> fetch_field()){
+            $a = $fieldinfo -> username;
+            $b = $fieldinfo -> table;
+            if ($a == $username && $b == password){
+                $_SESSION["loggedIn"] = true;
+                $c = $fieldinfo -> userid;
+                if ($c == 1)
+                    $admin = true;
+            }
+        }
+        $result -> free_result();
+        
+    }
+
+?>
+
+</body>
+</html>
+
 
