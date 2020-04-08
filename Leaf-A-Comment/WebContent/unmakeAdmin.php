338bc6579432ae8625d1eb3c2d1af22398009f25
nathania4c <nathania4c@gmail.com>
2020-04-08 23:22:15 +0700

    edits

--- Leaf-A-Comment/WebContent/unmakeAdmin.php
+++ Leaf-A-Comment/WebContent/unmakeAdmin.php
@@ -1,1 +1,28 @@
+<?php
+    
+    session_start();
+    
+    include "loggedin.php";
+    
+    $mysqli = new mysqli("localhost", "root","", "test");
+    
+    if($mysqli -> connect_errno){
+        die("Connection failed: " . $mysqli->connect_error);
+    } 
+    
+    if (isset($_GET["uid"])) {
+        
+        if ($stmt = $mysqli -> prepare("DELETE FROM admins WHERE uid = ?")){
+            
+            $stmt -> bind_param("i",$_GET["uid"]);
+            $stmt -> execute();
+            $stmt -> close();
+        
+        }
+    }
+    
+    header("Location: adminIndex.php");
+    exit;
+	
+?>
 
